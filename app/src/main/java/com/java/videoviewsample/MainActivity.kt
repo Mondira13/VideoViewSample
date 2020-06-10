3f0f939ea6eab957cf2a15185780861b84a1aa7f
Mondira Pal <mondira.p@encoders.co.in>
2020-06-10 12:57:16 +0530

    Add : play video using Exo player

---
+++ app/src/main/java/com/java/videoviewsample/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.java.videoviewsample
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.TrackGroupArray
+import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+
+class MainActivity : AppCompatActivity(), Player.EventListener  {
+    private lateinit var simpleExoplayer: SimpleExoPlayer
+    private var playbackPosition = 0L
+    private val dashUrl = "http://rdmedia.bbc.co.uk/dash/ondemand/bbb/2/client_manifest-separate_init.mpd"
+    private val bandwidthMeter by lazy {
+        DefaultBandwidthMeter()
+    }
+    private val adaptiveTrackSelectionFactory by lazy {
+        AdaptiveTrackSelection.Factory(bandwidthMeter)
+    }
+
+    private lateinit var simpleExoPlayerView : SimpleExoPlayerView
+    private lateinit var progressBar : ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        simpleExoPlayerView = findViewById(R.id.simpleExoPlayerView)
+        progressBar = findViewById(R.id.progressBar)
+    }
+
+    override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters?) {
+    }
+
+    override fun onTracksChanged(trackGroups: TrackGroupArray?, trackSelections: TrackSelectionArray?) {
+    }
+
+    override fun onPlayerError(error: ExoPlaybackException?) {
+    }
+
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        if (playbackState == Player.STATE_BUFFERING)
+            progressBar.visibility = View.VISIBLE
+        else if (playbackState == Player.STATE_READY)
+            progressBar.visibility = View.INVISIBLE
+    }
+
+    override fun onSeekProcessed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadingChanged(isLoading: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPositionDiscontinuity(reason: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onShuffleModeEnabledChanged(shuffleModeEnabled: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRepeatModeChanged(repeatMode: Int) {
+    }
+
+    override fun onTimelineChanged(timeline: Timeline?, manifest: Any?, reason: Int) {
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initializeExoplayer()
+    }
+
+    override fun onStop() {
+        releaseExoplayer()
+        super.onStop()
+    }
+
+    private fun initializeExoplayer() {
+        simpleExoplayer = ExoPlayerFactory.newSimpleInstance(
+                DefaultRenderersFactory(this),
+                DefaultTrackSelector(adaptiveTrackSelectionFactory),
+                DefaultLoadControl()
+        )
+
+        prepareExoplayer()
+        simpleExoPlayerView.player = simpleExoplayer
+        simpleExoplayer.seekTo(playbackPosition)
+        simpleExoplayer.playWhenReady = true
+        simpleExoplayer.addListener(this)
+    }
+
+    private fun releaseExoplayer() {
+        playbackPosition = simpleExoplayer.currentPosition
+        simpleExoplayer.release()
+    }
+
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        val dataSourceFactory = DefaultHttpDataSourceFactory("ua", bandwidthMeter)
+        val dashChunkSourceFactory = DefaultDashChunkSource.Factory(dataSourceFactory)
+        return DashMediaSource(uri, dataSourceFactory, dashChunkSourceFactory, null, null)
+    }
+
+    private fun prepareExoplayer() {
+        val uri = Uri.parse(dashUrl)
+        val mediaSource = buildMediaSource(uri)
+        simpleExoplayer.prepare(mediaSource)
+    }
+
+
+
+
+
+
+
+
+}
